d07f4fb243ced170b5c47b4a34da4ba5bdc088cb
Karu97 <karunanayake234@gmail.com>
2024-08-07 00:27:51 +0530

    Test

--- wp-content/themes/pinnwebs/header.php
+++ wp-content/themes/pinnwebs/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?> | <?php wp_title(''); ?></title>
   <!-- #CSS -->
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <!-- #box icon -->
   <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
   <!-- #remix icon -->
